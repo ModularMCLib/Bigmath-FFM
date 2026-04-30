31ea8b8bf925c701567a66611a6d8a170102485d
qiuye2024github <qiuye2024github@163.com>
2026-04-30 19:25:22 +0800

    Add PR test workflow and comprehensive test coverage
    build-on-push.yml: now installs GMP/MPFR and runs tests on Linux
    test-on-push.yml: auto-applies 'Tests: Passed'/'Tests: Failed' labels
    
    BigInt tests (28): fromLong, add, sub, mul, div, mod, pow, negate, abs,
      compare, signum, gcd, lcm, sqrt, and/or/xor, shift, isProbablePrime,
      factorial, nextPrime, fromString, fromBigInteger, formatting
    
    BigDecimal tests (20): fromDouble, fromString, add, sub, mul, div, negate,
      abs, sqrt, pow, log, exp, sin, cos, tan, ceil, floor, round, compare,
      formatting
    
    labeler.yml: added type:tests rule for src/test/** changes

--- src/test/java/net/modularmclib/bigmath/ffm/BigDecimalTest.java
+++ src/test/java/net/modularmclib/bigmath/ffm/BigDecimalTest.java
@@ -13,11 +13,28 @@ class BigDecimalTest {
 	}
 
 	@Test
+	void fromString() {
+		try (BigDecimal bd = BigDecimal.fromString("3.14", 64)) {
+			assertEquals(3.14, bd.toDouble(), 1e-10);
+		}
+	}
+
+	@Test
 	void add() {
 		try (BigDecimal a = BigDecimal.fromDouble(1.5, 64);
 			BigDecimal b = BigDecimal.fromDouble(2.5, 64)) {
 			try (BigDecimal c = a.add(b)) {
 				assertEquals(4.0, c.toDouble(), 1e-10);
+			}
+		}
+	}
+
+	@Test
+	void subtract() {
+		try (BigDecimal a = BigDecimal.fromDouble(5.0, 64);
+			BigDecimal b = BigDecimal.fromDouble(3.5, 64)) {
+			try (BigDecimal c = a.subtract(b)) {
+				assertEquals(1.5, c.toDouble(), 1e-10);
 			}
 		}
 	}
@@ -39,6 +56,156 @@ class BigDecimalTest {
 			try (BigDecimal c = a.divide(b)) {
 				assertEquals(10.0 / 3.0, c.toDouble(), 1e-10);
 			}
+		}
+	}
+
+	@Test
+	void negate() {
+		try (BigDecimal a = BigDecimal.fromDouble(3.14, 64)) {
+			try (BigDecimal b = a.negate()) {
+				assertEquals(-3.14, b.toDouble(), 1e-8);
+			}
+		}
+	}
+
+	@Test
+	void abs() {
+		try (BigDecimal a = BigDecimal.fromDouble(-3.14, 64)) {
+			try (BigDecimal b = a.abs()) {
+				assertEquals(3.14, b.toDouble(), 1e-8);
+			}
+		}
+	}
+
+	@Test
+	void absOfPositive() {
+		try (BigDecimal a = BigDecimal.fromDouble(3.14, 64)) {
+			try (BigDecimal b = a.abs()) {
+				assertEquals(3.14, b.toDouble(), 1e-8);
+			}
+		}
+	}
+
+	@Test
+	void sqrt() {
+		try (BigDecimal a = BigDecimal.fromDouble(100.0, 64)) {
+			try (BigDecimal c = a.sqrt()) {
+				assertEquals(10.0, c.toDouble(), 1e-10);
+			}
+		}
+	}
+
+	@Test
+	void pow() {
+		try (BigDecimal base = BigDecimal.fromDouble(2.0, 64);
+			BigDecimal exp = BigDecimal.fromDouble(10.0, 64)) {
+			try (BigDecimal c = base.pow(exp)) {
+				assertEquals(Math.pow(2.0, 10.0), c.toDouble(), 1e-6);
+			}
+		}
+	}
+
+	@Test
+	void log() {
+		try (BigDecimal a = BigDecimal.fromDouble(Math.E, 64)) {
+			try (BigDecimal c = a.log()) {
+				assertEquals(1.0, c.toDouble(), 1e-10);
+			}
+		}
+	}
+
+	@Test
+	void exp() {
+		try (BigDecimal a = BigDecimal.fromDouble(1.0, 64)) {
+			try (BigDecimal c = a.exp()) {
+				assertEquals(Math.E, c.toDouble(), 1e-10);
+			}
+		}
+	}
+
+	@Test
+	void sin() {
+		try (BigDecimal a = BigDecimal.fromDouble(Math.PI / 2, 64)) {
+			try (BigDecimal c = a.sin()) {
+				assertEquals(1.0, c.toDouble(), 1e-8);
+			}
+		}
+	}
+
+	@Test
+	void cos() {
+		try (BigDecimal a = BigDecimal.fromDouble(0.0, 64)) {
+			try (BigDecimal c = a.cos()) {
+				assertEquals(1.0, c.toDouble(), 1e-8);
+			}
+		}
+	}
+
+	@Test
+	void tan() {
+		try (BigDecimal a = BigDecimal.fromDouble(0.0, 64)) {
+			try (BigDecimal c = a.tan()) {
+				assertEquals(0.0, c.toDouble(), 1e-8);
+			}
+		}
+	}
+
+	@Test
+	void ceil() {
+		try (BigDecimal a = BigDecimal.fromDouble(3.14, 64)) {
+			try (BigDecimal c = a.ceil()) {
+				assertEquals(4.0, c.toDouble(), 1e-10);
+			}
+		}
+	}
+
+	@Test
+	void floor() {
+		try (BigDecimal a = BigDecimal.fromDouble(3.14, 64)) {
+			try (BigDecimal c = a.floor()) {
+				assertEquals(3.0, c.toDouble(), 1e-10);
+			}
+		}
+	}
+
+	@Test
+	void round() {
+		try (BigDecimal a = BigDecimal.fromDouble(3.6, 64)) {
+			try (BigDecimal c = a.round()) {
+				assertEquals(4.0, c.toDouble(), 1e-10);
+			}
+		}
+	}
+
+	@Test
+	void compare() {
+		try (BigDecimal a = BigDecimal.fromDouble(1.0, 64);
+			BigDecimal b = BigDecimal.fromDouble(2.0, 64)) {
+			assertTrue(a.compareTo(b) < 0);
+			assertTrue(b.compareTo(a) > 0);
+		}
+	}
+
+	@Test
+	void fromBigInt() {
+		try (BigInt bi = BigInt.fromLong(12345)) {
+			try (BigDecimal bd = BigDecimal.fromBigInt(bi, 64)) {
+				assertEquals(12345.0, bd.toDouble(), 1e-6);
+			}
+		}
+	}
+
+	@Test
+	void formatting() {
+		try (BigDecimal bd = BigDecimal.fromString("1234567.89", 64)) {
+			assertEquals("1,234,567.89", bd.toFormattedString());
+		}
+	}
+
+	@Test
+	void formattingWithScale() {
+		try (BigDecimal bd = BigDecimal.fromString("3.14159", 64)) {
+			assertEquals("3.14", bd.toFormattedString(2));
 		}
 	}
 }
--- src/test/java/net/modularmclib/bigmath/ffm/BigIntTest.java
+++ src/test/java/net/modularmclib/bigmath/ffm/BigIntTest.java
@@ -13,6 +13,20 @@ class BigIntTest {
 	}
 
 	@Test
+	void fromLongNegative() {
+		try (BigInt bi = BigInt.fromLong(-42)) {
+			assertEquals("-42", bi.toString());
+		}
+	}
+
+	@Test
+	void fromLongZero() {
+		try (BigInt bi = BigInt.fromLong(0)) {
+			assertEquals("0", bi.toString());
+		}
+	}
+
+	@Test
 	void add() {
 		try (BigInt a = BigInt.fromLong(10); BigInt b = BigInt.fromLong(32)) {
 			try (BigInt c = a.add(b)) {
@@ -49,6 +63,223 @@ class BigIntTest {
 	}
 
 	@Test
+	void mod() {
+		try (BigInt a = BigInt.fromLong(100); BigInt b = BigInt.fromLong(30)) {
+			try (BigInt c = a.mod(b)) {
+				assertEquals("10", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void modExactDivision() {
+		try (BigInt a = BigInt.fromLong(100); BigInt b = BigInt.fromLong(25)) {
+			try (BigInt c = a.mod(b)) {
+				assertEquals("0", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void pow() {
+		try (BigInt a = BigInt.fromLong(2)) {
+			try (BigInt c = a.pow(10)) {
+				assertEquals("1024", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void powZero() {
+		try (BigInt a = BigInt.fromLong(5)) {
+			try (BigInt c = a.pow(0)) {
+				assertEquals("1", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void negate() {
+		try (BigInt a = BigInt.fromLong(42)) {
+			try (BigInt b = a.negate()) {
+				assertEquals("-42", b.toString());
+			}
+		}
+	}
+
+	@Test
+	void negateTwice() {
+		try (BigInt a = BigInt.fromLong(42)) {
+			try (BigInt b = a.negate(); BigInt c = b.negate()) {
+				assertEquals("42", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void abs() {
+		try (BigInt a = BigInt.fromLong(-42)) {
+			try (BigInt b = a.abs()) {
+				assertEquals("42", b.toString());
+			}
+		}
+	}
+
+	@Test
+	void absOfPositive() {
+		try (BigInt a = BigInt.fromLong(42)) {
+			try (BigInt b = a.abs()) {
+				assertEquals("42", b.toString());
+			}
+		}
+	}
+
+	@Test
+	void compare() {
+		try (BigInt a = BigInt.fromLong(10); BigInt b = BigInt.fromLong(20)) {
+			assertTrue(a.compareTo(b) < 0);
+			assertTrue(b.compareTo(a) > 0);
+			assertEquals(0, a.compareTo(a));
+		}
+	}
+
+	@Test
+	void signum() {
+		try (BigInt a = BigInt.fromLong(-5)) {
+			assertEquals(-1, a.signum());
+		}
+		try (BigInt a = BigInt.fromLong(0)) {
+			assertEquals(0, a.signum());
+		}
+		try (BigInt a = BigInt.fromLong(5)) {
+			assertEquals(1, a.signum());
+		}
+	}
+
+	@Test
+	void gcd() {
+		try (BigInt a = BigInt.fromLong(48); BigInt b = BigInt.fromLong(18)) {
+			try (BigInt c = a.gcd(b)) {
+				assertEquals("6", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void gcdOfSelf() {
+		try (BigInt a = BigInt.fromLong(7)) {
+			try (BigInt c = a.gcd(a)) {
+				assertEquals("7", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void lcm() {
+		try (BigInt a = BigInt.fromLong(12); BigInt b = BigInt.fromLong(18)) {
+			try (BigInt c = a.lcm(b)) {
+				assertEquals("36", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void sqrt() {
+		try (BigInt a = BigInt.fromLong(100)) {
+			try (BigInt c = a.sqrt()) {
+				assertEquals("10", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void sqrtNonPerfect() {
+		try (BigInt a = BigInt.fromLong(101)) {
+			try (BigInt c = a.sqrt()) {
+				assertEquals("10", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void bitwiseAnd() {
+		try (BigInt a = BigInt.fromLong(0xFF0F); BigInt b = BigInt.fromLong(0xF0FF)) {
+			try (BigInt c = a.and(b)) {
+				assertEquals(Integer.toString(0xFF0F & 0xF0FF), c.toString());
+			}
+		}
+	}
+
+	@Test
+	void bitwiseOr() {
+		try (BigInt a = BigInt.fromLong(0xFF0F); BigInt b = BigInt.fromLong(0xF0FF)) {
+			try (BigInt c = a.or(b)) {
+				assertEquals(Integer.toString(0xFF0F | 0xF0FF), c.toString());
+			}
+		}
+	}
+
+	@Test
+	void bitwiseXor() {
+		try (BigInt a = BigInt.fromLong(0xFF0F); BigInt b = BigInt.fromLong(0xF0FF)) {
+			try (BigInt c = a.xor(b)) {
+				assertEquals(Integer.toString(0xFF0F ^ 0xF0FF), c.toString());
+			}
+		}
+	}
+
+	@Test
+	void shiftLeft() {
+		try (BigInt a = BigInt.fromLong(1)) {
+			try (BigInt c = a.shiftLeft(10)) {
+				assertEquals("1024", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void shiftRight() {
+		try (BigInt a = BigInt.fromLong(1024)) {
+			try (BigInt c = a.shiftRight(3)) {
+				assertEquals("128", c.toString());
+			}
+		}
+	}
+
+	@Test
+	void isProbablyPrime() {
+		try (BigInt a = BigInt.fromLong(17)) {
+			assertTrue(a.isProbablyPrime(10));
+		}
+		try (BigInt a = BigInt.fromLong(100)) {
+			assertFalse(a.isProbablyPrime(10));
+		}
+	}
+
+	@Test
+	void factorial() {
+		try (BigInt f = BigInt.factorial(10)) {
+			assertEquals("3628800", f.toString());
+		}
+	}
+
+	@Test
+	void factorialZero() {
+		try (BigInt f = BigInt.factorial(0)) {
+			assertEquals("1", f.toString());
+		}
+	}
+
+	@Test
+	void nextPrime() {
+		try (BigInt a = BigInt.fromLong(20)) {
+			try (BigInt c = a.nextPrime()) {
+				assertEquals("23", c.toString());
+			}
+		}
+	}
+
+	@Test
 	void fromString() {
 		try (BigInt bi = BigInt.fromString("12345678901234567890", 10)) {
 			assertEquals("12345678901234567890", bi.toString());
@@ -63,12 +294,48 @@ class BigIntTest {
 	}
 
 	@Test
+	void fromBigInteger() {
+		var javaBi = new java.math.BigInteger("99999999999999999999");
+		try (BigInt bi = BigInt.fromBigInteger(javaBi)) {
+			assertEquals(javaBi.toString(), bi.toString());
+		}
+	}
+
+	@Test
+	void toBigInteger() {
+		try (BigInt bi = BigInt.fromString("12345678901234567890", 10)) {
+			assertEquals(new java.math.BigInteger("12345678901234567890"), bi.toBigInteger());
+		}
+	}
+
+	@Test
 	void largeMultiplication() {
 		try (BigInt a = BigInt.fromString("99999999999999999999", 10);
 			BigInt b = BigInt.fromString("99999999999999999999", 10)) {
 			try (BigInt c = a.multiply(b)) {
 				assertEquals("9999999999999999999800000000000000000001", c.toString());
 			}
+		}
+	}
+
+	@Test
+	void formatting() {
+		try (BigInt bi = BigInt.fromString("1234567", 10)) {
+			assertEquals("1,234,567", bi.toFormattedString());
+		}
+	}
+
+	@Test
+	void formattingLarge() {
+		try (BigInt bi = BigInt.fromString("12345678901234567890", 10)) {
+			assertEquals("12,345,678,901,234,567,890", bi.toFormattedString());
+		}
+	}
+
+	@Test
+	void formattingCustom() {
+		try (BigInt bi = BigInt.fromString("12345678", 10)) {
+			assertEquals("1234 5678", bi.toFormattedString(4, " "));
 		}
 	}
 }
