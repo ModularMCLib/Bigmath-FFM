9170d80b79f3d1b1a22ec13a032ec6e4486fc040
qiuye2024github <qiuye2024github@163.com>
2026-05-01 15:54:34 +0800

    docs: add Javadoc to Java files and KDoc to Kotlin operator files

--- bigmath-ffm-kt/src/main/kotlin/com/modularmc/bigmath/ffm/BigIntOperators.kt
+++ bigmath-ffm-kt/src/main/kotlin/com/modularmc/bigmath/ffm/BigIntOperators.kt
@@ -1,16 +1,28 @@
 package com.modularmc.bigmath.ffm
 
+/** Arithmetic addition operator. */
 operator fun BigInt.plus(other: BigInt): BigInt = add(other)
+/** Arithmetic subtraction operator. */
 operator fun BigInt.minus(other: BigInt): BigInt = subtract(other)
+/** Arithmetic multiplication operator. */
 operator fun BigInt.times(other: BigInt): BigInt = multiply(other)
+/** Arithmetic division operator (integer division, truncating toward zero). */
 operator fun BigInt.div(other: BigInt): BigInt = divide(other)
+/** Remainder (modulus) operator. */
 operator fun BigInt.rem(other: BigInt): BigInt = mod(other)
+/** Unary negation operator. */
 operator fun BigInt.unaryMinus(): BigInt = negate()
+/** Unary plus operator (identity). */
 operator fun BigInt.unaryPlus(): BigInt = this
+/** Increment operator ({@code ++x}). */
 operator fun BigInt.inc(): BigInt = add(BigInt.ONE)
+/** Decrement operator ({@code --x}). */
 operator fun BigInt.dec(): BigInt = subtract(BigInt.ONE)
+/** Left bitwise shift operator ({@code shl}). */
 infix fun BigInt.shl(bits: Int): BigInt = shiftLeft(bits.toLong())
+/** Right bitwise shift operator ({@code shr}). */
 infix fun BigInt.shr(bits: Int): BigInt = shiftRight(bits.toLong())
+/** Range-to operator ({@code ..}) producing a closed range. */
 operator fun BigInt.rangeTo(other: BigInt): ClosedRange<BigInt> {
     val self = this
     return object : ClosedRange<BigInt> {
--- bigmath-ffm-kt/src/main/kotlin/com/modularmc/bigmath/ffm/Int128Operators.kt
+++ bigmath-ffm-kt/src/main/kotlin/com/modularmc/bigmath/ffm/Int128Operators.kt
@@ -1,14 +1,24 @@
 package com.modularmc.bigmath.ffm
 
+/** Arithmetic addition operator. */
 operator fun Int128.plus(other: Int128): Int128 = add(other)
+/** Arithmetic subtraction operator. */
 operator fun Int128.minus(other: Int128): Int128 = subtract(other)
+/** Arithmetic multiplication operator. */
 operator fun Int128.times(other: Int128): Int128 = multiply(other)
+/** Arithmetic division operator (truncating toward zero). */
 operator fun Int128.div(other: Int128): Int128 = divide(other)
+/** Remainder (modulus) operator. */
 operator fun Int128.rem(other: Int128): Int128 = mod(other)
+/** Unary negation operator. */
 operator fun Int128.unaryMinus(): Int128 = negate()
+/** Unary plus operator (identity). */
 operator fun Int128.unaryPlus(): Int128 = this
+/** Increment operator ({@code ++x}). */
 operator fun Int128.inc(): Int128 = add(Int128.ONE)
+/** Decrement operator ({@code --x}). */
 operator fun Int128.dec(): Int128 = subtract(Int128.ONE)
+/** Range-to operator ({@code ..}) producing a closed range. */
 operator fun Int128.rangeTo(other: Int128): ClosedRange<Int128> {
     val self = this
     return object : ClosedRange<Int128> {
