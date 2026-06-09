11b29a1dd15e73effc3d61539428aab5af2b1229
QiuYe <qiuye2024github@163.com>
2026-06-09 13:20:47 +0800

    Optimize native BigDeci single separator formatting

--- src/main/cpp/bigdecimal.cpp
+++ src/main/cpp/bigdecimal.cpp
@@ -218,8 +218,12 @@ char *bigdecimal_format(mpfr_ptr a, int scale, int group_size, const char *group
 	size_t next_group = first_group;
 	for (size_t i = 0; i < int_len; i++) {
 		if (sep_len > 0 && i == next_group) {
-			memcpy(out + pos, group_sep, sep_len);
-			pos += sep_len;
+			if (sep_len == 1) {
+				out[pos++] = group_sep[0];
+			} else {
+				memcpy(out + pos, group_sep, sep_len);
+				pos += sep_len;
+			}
 			next_group += (size_t)group_size;
 		}
 		out[pos++] = (exp > 0 && i < digit_len) ? p[i] : '0';
