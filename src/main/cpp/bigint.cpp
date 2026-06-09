bd02c3e1ab36e24892cf62055c5d9b340c47bfe0
QiuYe <qiuye2024github@163.com>
2026-06-09 13:08:52 +0800

    Revert "Optimize native BigInt factorial"
    This reverts commit c231b2f78600635a914f42cb8f5cf684816441ce.

--- src/main/cpp/bigint.cpp
+++ src/main/cpp/bigint.cpp
@@ -330,7 +330,7 @@ void bigint_factorial(mpz_ptr *out, uint64_t n) {
 	*out = (mpz_ptr)malloc(sizeof(__mpz_struct));
 	if (!*out) return;
 	mpz_init(*out);
-	bigmath::product_tree_factorial(*out, n);
+	mpz_fac_ui(*out, n);
 }
 
 void bigint_next_prime(mpz_ptr *out, mpz_ptr a) {
